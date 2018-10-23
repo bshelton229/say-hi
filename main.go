3f4a5dbf258289f153aa46e94e526f0a770b942a
Bryan Shelton <bryan@rover.com>
2018-10-23 12:15:33 -0700

    Uplift grace period

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func main() {
 	log.Println("Shutting down server...")
 
 	// shut down gracefully, but wait no longer than 5 seconds before halting
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	srv.Shutdown(ctx)
 
 	log.Println("Server gracefully stopped")
